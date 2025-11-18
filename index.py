aa605eba892b1a1841aa4bfa101f84af5fb95558
Anderson Liew <liew.and@northeastern.edu>
2025-11-18 17:04:12 -0500

    Pytesseract was the wrong tool, will try EasyOCR

--- index.py
+++ index.py
@@ -1,1 +1,11 @@
-print("testing")
+import pytesseract
+
+from PIL import Image
+
+try:
+    img = Image.open('handwriting.png')
+
+    text = pytesseract.image_to_string(img)
+    print(text)
+except FileNotFoundError:
+    print("No such file or directory.")
